701a6b7a965811d9f358df13e99e33531c7eeb6f
Oliver Adams <oliver.adams@gmail.com>
2019-05-06 09:28:24 -0400

    Moved forced alignment / RTTM file generation code to another file.

--- uam/asr1/run.py
+++ uam/asr1/run.py
@@ -509,119 +509,6 @@ def kws(lang, env, re_index=True, custom_kwlist=True, filt_dtl=False):
             lang_dir, data_dir, decode_dir]
     run(args, check=True)
 
-def add_lang_suffixes_to_tokens(transcription_path, lang):
-    """ Replaces words in a Kaldi-style transcription with a suffix.
-
-    For example, given lang="202", the Babel Swahili utterance
-        10524_A_20131009_200043_004061 <hes> kuoa ndiyo nilikuwa
-        nimeoa
-    would become:
-        10524_A_20131009_200043_004061 <hes> kuoa_202 ndiyo_202 nilikuwa_202
-        nimeoa_202
-    Notice that the <hes> tag was unaffected. Things in angle brackets such as
-    <hes> and <noise> are ignored as they are considered language independent.
-    """
-
-    with open(transcription_path) as f:
-        lines = [line.split() for line in f]
-
-    with open(transcription_path, "w") as f:
-        for line in lines:
-            utt_id = line[0]
-            transcript_toks = []
-            for tok in line[1:]:
-                if tok.startswith("<") and tok.endswith(">"):
-                    transcript_toks.append(tok)
-                else:
-                    transcript_toks.append(f"{tok}_{lang}")
-            line_out = " ".join([utt_id] + transcript_toks)
-            print(line_out, file=f)
-
-def force_align(lang, args):
-    """
-
-    Note that if the evaluation keywords are in the dev10h set, there's no need
-    to do this force_align / generate_rttm() step, because there are gold
-    standard RTTM files listed in conf/lang.conf.
-    """
-
-    nj = str(args.decode_nj)
-
-    # TODO Retrain the tri5 model using both train and dev data in training so
-    # we can get better ground-truth alignments.
-
-    # TODO Possibly we want to rejig train data so that we have no OOV tokens
-    # in our dev set, so that we can get better ground truth for our newly
-    # generated KWS tasks.
-
-    # First perform forced alignment
-    # TODO Make sure this is the right alignment script. Can I just use what
-    # we used for HMM-GMM alignment in preprocessing?
-    test_set = f"{lang}_test"
-    data_dir = f"data/{test_set}/data/dev10h.pem"
-
-    # The multilingual training data suffixed each word with the lang-code, to
-    # make language-specific representations of homographs across languages.
-    # However, the dev transcription data does not include such word suffixes,
-    # so there is a lexicon mismatch. One solution would be to change how trainin
-    # graphs are compiled. Another approach is instead to create a new version
-    # of the dev data directory where we process the text to have the language
-    # suffixes. This is the approach we take here.
-
-    # Make a copy of the data directory so that we can modify the text to be
-    # compatible with the training graphs.
-    lang_suffix_data_dir = f"{data_dir}.lang_suffix"
-    args = ["rsync", "-av", f"{data_dir}/", lang_suffix_data_dir]
-    del data_dir # So we don't accidentally reference it.
-    run(args, check=True)
-    # Delete the data splits in that directory that have the old token form.
-    split_nj_path = Path(f"{lang_suffix_data_dir}/split{nj}")
-    if split_nj_path.exists():
-        args = ["rm", "-r", str(split_nj_path)]
-        run(args, check=True)
-    # 3. Replace word tokens with word_{lang}
-    add_lang_suffixes_to_tokens(Path(lang_suffix_data_dir) / "text", lang)
-
-    # NOTE This was the model used in getting the initial alignments for
-    # training.
-    lang_dir = "data/lang_universalp/tri5"
-    # TODO Exp dir an ali dir will have to change to be relevant to the
-    # specific language at hand.
-    src_dir = f"exp/tri5" # The directory that has the AM we'll use for alignment.
-    ali_dir = f"exp/tri5_ali_{test_set}_dev10h" # TODO Really need to factor out dev10h
-
-    cmd = "utils/queue.pl --mem 10G"
-    args = ["steps/align_fmllr.sh",
-            "--nj", nj,
-            "--cmd", cmd,
-            lang_suffix_data_dir, lang_dir, src_dir, ali_dir]
-    run(args, check=True)
-
-def generate_rttm(lang, args):
-    """
-    """
-
-    test_set = f"{lang}_test"
-
-    # Now produce an RTTM file from the alignments
-    # TODO Generalize these.
-    #data_dir = f"data/{test_set}/data/dev10h.pem"
-    #lang_dir = f"data/{test_set}/data/lang_universal"
-    #ali_dir = f""
-    data_dir = f"data/{test_set}/data/dev10h.pem"
-    lang_suffix_data_dir = f"{data_dir}.lang_suffix"
-    lang_dir = "data/lang_universalp/tri5"
-    ali_dir = f"exp/tri5_ali_{test_set}_dev10h"
-    args = ["local/ali_to_rttm.sh",
-            lang_suffix_data_dir, lang_dir, ali_dir]
-    run(args, check=True)
-
-    # NOTE In order to have a lexicon that dealt with things like t_"_B, I
-    # needed to run cut -d " " -f 2- \
-    # data/lang_universalp/tri5/phones/align_lexicon.txt > \
-    # data/lang_universalp/tri5/phones/lexicon.txt
-    # On top of this, the call in ali_to_rttm.sh to local/make_L_align.sh had
-    # to change the first argument to data/lang_universalp/tri5/phones
 
 def create_unimorph_babel_kwlist(lang):
     """ Given Unimorph and Babel data for a language, create a keyword list for
@@ -733,5 +620,6 @@ if __name__ == "__main__":
     # because our eval sets are all based on the Babel dev10h, for which there
     # are RTTM files on the grid. See conf/lang/*.conf for paths.
     ##### Creating RTTM #####
-    #force_align(args.test_lang, args)
-    #generate_rttm(args.test_lang, args)
+    import create_eval_set
+    #create_eval_set.force_align(args.test_lang, args)
+    #create_eval_set.generate_rttm(args.test_lang, args)
